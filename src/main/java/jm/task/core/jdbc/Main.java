9a0541b2e1d51764008cba1bb8bae50b1db2695b
IPESHKUR <147414639+IPESHKUR@users.noreply.github.com>
2023-12-29 19:10:44 +0300

    Practice 1.1.5

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,9 +23,9 @@ public class Main {
         }
         System.out.println(userImpl.getAllUsers());
         userImpl.removeUserById(4);
-//        userImpl.cleanUsersTable();
-//        userImpl.dropUsersTable();
-//        Util.closeSessionFactory();
+        userImpl.cleanUsersTable();
+        userImpl.dropUsersTable();
+        Util.closeSessionFactory();
 
 
     }
